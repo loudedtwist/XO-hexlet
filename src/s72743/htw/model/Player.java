f9442e261b94323f2e8e34292a3daa21c1e5a7ba
warik <loudedtwist@gmail.com>
2015-09-15 21:43:37 +0200

    Player created

---
+++ src/s72743/htw/model/Player.java
@@ -0,0 +1,19 @@
+package s72743.htw.model;
+
+
+public class Player {
+    private final String name;
+    private final Figure figure;
+
+     public Player(final String name, final Figure figure){
+        this.name=name;
+        this.figure=figure;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public Figure getFigure(){
+        return figure;
+    }
+}
