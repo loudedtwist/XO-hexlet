b55c49854751ab12225b1d93dd5e1186ccdcd048
warik <loudedtwist@gmail.com>
2015-09-16 00:56:35 +0200

    Field - Method to set und get Figure

--- src/s72743/htw/model/Field.java
+++ src/s72743/htw/model/Field.java
@@ -1,7 +1,7 @@
 package s72743.htw.model;
 
 import s72743.htw.model.exceptions.AlreadyOccupiedException;
-import s72743.htw.model.exceptions.InvalidePointException;
+import s72743.htw.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
@@ -17,21 +17,21 @@ public class Field {
     public int getSize(){
         return FIELD_SIZE*FIELD_SIZE;
     }
-    public Figure getFigure(final Point point) throws InvalidePointException {
-        if (!checkPoint(point)) throw new InvalidePointException();
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) throw new InvalidPointException();
         return figure[point.y][point.x];
     }
 
     public void setFigure (final Point point, final Figure figure)
-            throws InvalidePointException,
+            throws InvalidPointException,
                     AlreadyOccupiedException {
-        if (!checkPoint(point)) throw new InvalidePointException();
+        if (!checkPoint(point)) throw new InvalidPointException();
         if(this.figure[point.y][point.x] != null) throw new AlreadyOccupiedException();
         this.figure[point.y][point.x] = figure;
     }
 
     private boolean checkPoint(final Point point){
-        return ((point.y >= MIN_FIELDVALUE || point.y < FIELD_SIZE) && (point.x >= MIN_FIELDVALUE || point.y < FIELD_SIZE) );
+        return ((point.y >= MIN_FIELDVALUE && point.y < FIELD_SIZE) && (point.x >= MIN_FIELDVALUE && point.x < FIELD_SIZE) );
 
     }
 
