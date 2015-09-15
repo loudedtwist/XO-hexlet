fedf9282babe5c0076c824e86608d79134989ad6
warik <loudedtwist@gmail.com>
2015-09-16 00:55:53 +0200

    AlreadyOccupiedException - the spelling was improved

--- src/s72743/htw/model/exceptions/AlreadyOccupiedException.java
+++ src/s72743/htw/model/exceptions/AlreadyOccupiedException.java
@@ -1,7 +1,4 @@
 package s72743.htw.model.exceptions;
 
-/**
- * Created by warik on 16.09.15.
- */
 public class AlreadyOccupiedException extends AbstractXOException {
 }
---
+++ src/s72743/htw/model/exceptions/InvalidPointException.java
@@ -0,0 +1,4 @@
+package s72743.htw.model.exceptions;
+
+public class InvalidPointException extends AbstractXOException {
+}
