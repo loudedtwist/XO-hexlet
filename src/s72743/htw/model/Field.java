a735dd89d1ef2a3479f271d3aa6e071bf57cf847
warik <loudedtwist@gmail.com>
2015-09-16 00:32:39 +0200

    Field - tests to check the Point coordinats added

--- src/s72743/htw/model/Field.java
+++ src/s72743/htw/model/Field.java
@@ -1,10 +1,14 @@
 package s72743.htw.model;
 
+import s72743.htw.model.exceptions.AlreadyOccupiedException;
+import s72743.htw.model.exceptions.InvalidePointException;
+
 import java.awt.*;
 
 public class Field {
 
     public static final int FIELD_SIZE = 3;
+    public static final int MIN_FIELDVALUE = 0;
     private Figure figure[][] = new Figure[FIELD_SIZE][FIELD_SIZE];
 
 
@@ -13,16 +17,23 @@ public class Field {
     public int getSize(){
         return FIELD_SIZE*FIELD_SIZE;
     }
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidePointException {
+        if (!checkPoint(point)) throw new InvalidePointException();
         return figure[point.y][point.x];
     }
 
-    public void setFigure (final Point point, final Figure figure) {
-        if((point.y < 0 || point.y > 2) || (point.x < 0 || point.x > 2) )
-           return;
+    public void setFigure (final Point point, final Figure figure)
+            throws InvalidePointException,
+                    AlreadyOccupiedException {
+        if (!checkPoint(point)) throw new InvalidePointException();
+        if(this.figure[point.y][point.x] != null) throw new AlreadyOccupiedException();
         this.figure[point.y][point.x] = figure;
     }
 
+    private boolean checkPoint(final Point point){
+        return ((point.y >= MIN_FIELDVALUE || point.y < FIELD_SIZE) && (point.x >= MIN_FIELDVALUE || point.y < FIELD_SIZE) );
+
+    }
 
 
 
