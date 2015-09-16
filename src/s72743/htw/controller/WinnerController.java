3ef7adb0e8401410a261b3382794506733453ec2
warik <loudedtwist@gmail.com>
2015-09-16 22:18:14 +0200

    WinnerController:  1. new funktion with lambda from java 8

--- src/s72743/htw/controller/WinnerController.java
+++ src/s72743/htw/controller/WinnerController.java
@@ -7,7 +7,7 @@ import s72743.htw.model.exceptions.InvalidPointException;
 import java.awt.*;
 
 public class WinnerController {
-    public Figure getWinner(Field field){
+    public Figure getWinner3x3(Field field){
 
             try {
                 //updown lines
@@ -40,6 +40,25 @@ public class WinnerController {
                 e.printStackTrace();
             }
         return null;
+    }
+    public Figure getWinner(Field field){
+        //rows
+        try {
+            for(int i= 0; i<field.getSize();i++)
+            if(check(field, new Point(0, i), p -> new Point(p.x+1,p.y)))
+                return field.getFigure(new Point(0, i));
+            for(int i= 0; i<field.getSize();i++)
+            if(check(field, new Point(i, 0), p -> new Point(p.x,p.y+1)))
+                return field.getFigure(new Point(i, 0));
+            if(check(field, new Point(0, 0), p -> new Point(p.x+1,p.y+1)))
+                return field.getFigure(new Point(0, 0));
+            if(check(field, new Point(0, 2), p -> new Point(p.x+1,p.y-1)))
+                return field.getFigure(new Point(1, 1));
+            return null;
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+
     }
     private boolean check(final Field field, final Point currentPoint,final IPointGenerator pointGenerator){
         final Figure currentFigure;
