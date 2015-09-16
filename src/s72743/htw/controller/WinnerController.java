7149098665f3ae7dcbbdcef4306b6f97b9c0a6a8
warik <loudedtwist@gmail.com>
2015-09-16 19:36:59 +0200

    WinnerController:  1. recursive  method check added  2. interface PointGenerator added

--- src/s72743/htw/controller/WinnerController.java
+++ src/s72743/htw/controller/WinnerController.java
@@ -6,35 +6,32 @@ import s72743.htw.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
-/**
- * Created by warik on 16.09.15.
- */
 public class WinnerController {
     public Figure getWinner(Field field){
 
             try {
                 //updown lines
-                for(int x = 0; x < 3; x++) {
-                    if (field.getFigure(new Point(x, 0)) == null) break;
+                for(int x = Field.getMinFieldvalue(); x < field.getSize(); x++) {
+                    if (field.getFigure(new Point(x, 0)) == null) return null;
                     if (field.getFigure(new Point(x, 0)) == field.getFigure(new Point(x, 1))
                             && field.getFigure(new Point(x, 1)) == field.getFigure(new Point(x, 2)))
                         return field.getFigure(new Point(x, 0));
                 }
                 //---lines
-                for(int y = 0; y < 3; y++) {
-                    if (field.getFigure(new Point(0, y)) == null) break;
+                for(int y = Field.getMinFieldvalue(); y < field.getSize(); y++) {
+                    if (field.getFigure(new Point(0, y)) == null) return null;
                     if (field.getFigure(new Point(0, y)) == field.getFigure(new Point(1, y))
                             && field.getFigure(new Point(1, y)) == field.getFigure(new Point(2, y)))
                         return field.getFigure(new Point(0, y));
                 }
                 //diagonal 1 \
-                if (field.getFigure(new Point(0, 0)) == null) break;
+                if (field.getFigure(new Point(0, 0)) == null) return null;
                 if (field.getFigure(new Point(0, 0)) == field.getFigure(new Point(1, 1))
                         && field.getFigure(new Point(1, 1)) == field.getFigure(new Point(2, 2)))
                     return field.getFigure(new Point(0, 0));
 
                 //diagonal 2 /
-                if (field.getFigure(new Point(0, 2)) == null) break;
+                if (field.getFigure(new Point(0, 2)) == null) return null;
                 if (field.getFigure(new Point(0, 2)) == field.getFigure(new Point(1, 1))
                         && field.getFigure(new Point(1, 1)) == field.getFigure(new Point(2, 0)))
                     return field.getFigure(new Point(0, 2));
@@ -42,5 +39,27 @@ public class WinnerController {
             } catch (InvalidPointException e) {
                 e.printStackTrace();
             }
+        return null;
+    }
+    private boolean check(final Field field, final Point currentPoint,final IPointGenerator pointGenerator){
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.nextPoint(currentPoint);
+
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            nextFigure = field.getFigure(nextPoint);
+        } catch (InvalidPointException e) {
+            return true;
+        }
+
+        if (currentFigure == null) return false;
+        if (currentFigure != nextFigure) return false;
+        return check(field, next, pointGenerator)
+
+
+    }
+    private interface IPointGenerator{
+        Point nextPoint(final Point point);
     }
 }
